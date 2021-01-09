654b49d7e630e2e6fa3bf292131f653b9a7919e9
natsumikan07 <m.natsumi07@gmail.com>
2021-01-09 23:44:37 +0900

    リポジトリの.gitの位置修正。最初にリポジトリ作成すべきだった。 （空のリポジトリ作って、そこにInitialzrで作ったものをコピーする）

---
+++ src/main/java/jp/co/bhopari/timetransform/TimeTransformApplication.java
@@ -0,0 +1,13 @@
+package jp.co.bhopari.timetransform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTransformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTransformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/bhopari/timetransform/TimeTransformApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.bhopari.timetransform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTransformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
